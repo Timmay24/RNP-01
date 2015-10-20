ac4c6891ee9f7627fcc07f6c7e7ce30ed3c4f2bd
timmay24 <tim.hartig@haw-hamburg.de>
2015-10-20 16:20:25 +0200

    Add correct path processing for attachment

--- src/de/haw/Main.java
+++ src/de/haw/Main.java
@@ -1,10 +1,15 @@
 package de.haw;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
 
     public static void main(String[] args) {
 //        MailFile mf = new MailFile(args[0], args[1]);
-        MailFile mf = new MailFile("tim.hartig@haw-hamburg.de", "foobar.txt");
+        Path currentRelativePath = Paths.get("");
+        String s = currentRelativePath.toAbsolutePath().toString();
+        MailFile mf = new MailFile("tim.hartig@haw-hamburg.de", s + "/docker.png");
         mf.sendMail();
     }
 }
