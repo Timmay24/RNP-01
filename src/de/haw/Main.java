ac184f105eaabf4f84166be5cd3ed295a6ae6086
timmay24 <tim.hartig@haw-hamburg.de>
2015-10-22 12:58:32 +0200

    Main arguments are no longer hard coded

--- src/de/haw/Main.java
+++ src/de/haw/Main.java
@@ -6,10 +6,15 @@ import java.nio.file.Paths;
 public class Main {
 
     public static void main(String[] args) {
-//        MailFile mf = new MailFile(args[0], args[1]);
         Path currentRelativePath = Paths.get("");
         String s = currentRelativePath.toAbsolutePath().toString();
-        MailFile mf = new MailFile("fabian.pfaff@haw-hamburg.de", s + "/docker.png");
-        mf.sendMail();
+        if (args.length == 2 && !args[0].isEmpty() && !args[1].isEmpty()) {
+            MailFile mf = new MailFile(args[0], args[1]);
+            mf.sendMail();
+//            MailFile mf = new MailFile("fabian.pfaff@haw-hamburg.de", s + "/docker.png");
+//            MailFile mf = new MailFile("tim.hartig@haw-hamburg.de", s + "/docker.png");
+        } else {
+            System.out.println("Illegal arguments. Script aborting.");
+        }
     }
 }
